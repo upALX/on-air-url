f00c44c7a49d30ef4e749da62de4d92995abca73
Alexandre <alexandreaddres@gmail.com>
2023-09-18 19:53:06 -0300

    adding treat of errors in js

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+import fs from 'node:fs';
 import chalk from 'chalk';
 
-console.log(chalk.blue('Hello world!'))
+
+function treatErrors(errorObject, filePath){
+    throw new Error(
+        chalk.red(errorObject.code, `Ocorreu um erro durante o processo de pegar o arquivo no caminho: ${filePath}`)
+    )
+}
+
+function getFile(filePath){
+    const encoding = 'utf-8'
+    fs.readFile(filePath, encoding, (error, text) => {
+
+        if (error){
+            treatErrors(error, filePath)
+        }
+        console.log(chalk.yellow(text))
+    })
+}
+
+getFile('./files/text')
