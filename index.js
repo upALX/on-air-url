caa8433a23e4929e7a543476603c083ba81b6512
Alexandre <alexandreaddres@gmail.com>
2023-09-20 23:09:23 -0300

    adding get url links tested to the project

--- index.js
+++ index.js
@@ -1,38 +1,57 @@
 import fs from 'node:fs';
 import chalk from 'chalk';
+import { type } from 'node:os';
 
-const text = "blablalbal[Custom containers](https://github.com/markdown-it/markdown-it-container)sdadassdsa Third paragraph of definition 2 [Definition lists](https://github.com/markdown-it/markdown-it-deflist)"
+const textRandom = "blablalbal[Custom containers](https://github.com/markdown-it/markdown-it-container)sdadassdsa Third paragraph of definition 2 [Definition lists](https://github.com/markdown-it/markdown-it-deflist)"
 
-const regPatternUrl = new RegExp(/\[([^[\]]*?)\]\((https?:\/\/[^s?#.].[^\s]*)\)/)
-const allResultPattern = [...text.matchAll(regPatternUrl)]
-const results = allResultPattern.map(response => ({
-    [response[1]]: response[2]
-}))
 
 
 function treatErrors(errorObject, filePath){
+    console.log('Error:', errorObject)
     throw new Error(
         chalk.red(errorObject.code, `There is an error on get the file on path: ${filePath}`)
     )
 }
 
-function getFile(filePath){
+async function getLinksFromText(filePath){
     const encoding = 'utf-8'
-    fs.readFile(filePath, encoding, (error, text) => {
 
-        if (error){
-            treatErrors(error, filePath)
-        }
+    try{
+        console.log('Try get text from file...')
 
-        console.log(chalk.yellow(text))
+        const textFromFile = await fs.promises.readFile(filePath, encoding)
 
-    })
+        console.log('TYPE OF CONTENT FILE: ', typeof(textFromFile))
+
+        const urlLinks = getUrlLinks(textFromFile)
+
+        console.log('Links from file: ', urlLinks)
+        
+        return urlLinks
+    }catch(error){
+        treatErrors(error, filePath)
+    }
+    console.log('Text from file: ', text)
 
 }
 
-const fileText = getFile('./files/text.md')
+function getUrlLinks(text){
+    const regPatternUrl = new RegExp(/\[([^[\]]*?)\]\((https?:\/\/[^s?#.].[^\s]*)\)/gm)
 
-console.log(fileText)
+    console.log('Getting and creating a Pattern object with constructor regEXP: ', regPatternUrl)
 
-//const matchPattern = 
+    const allResultPattern = [...text.matchAll(regPatternUrl)]
+
+    console.log()
+
+    const linksFromText = allResultPattern.map(response => ({
+        [response[1]]: response[2]
+    }))
+
+    return linksFromText
+}
+
+const textFromFile = getLinksFromText('./files/text.md')
+
+console.log('The text from file in global scope is: ', textFromFile)
 
