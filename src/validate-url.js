d19635a68520ebae91533baf06d492d6eb06cbd3
Alexandre <alexandreaddres@gmail.com>
2023-09-22 08:06:46 -0300

    Adding validation base of links in project. Tested.

--- src/validate-url.js
+++ src/validate-url.js
@@ -1,3 +1,30 @@
-export default function validateLinks(links){
-    return 'is on the function'
+function extractLinks(objectLinksToExtract){
+    return objectLinksToExtract.map(objectLink => Object.values(objectLink).join())
+}
+
+export default async function validateLinks(links){
+
+    const linksExtracted = extractLinks(links)
+
+    const listNotExists = []
+
+    try{
+        for (let link of linksExtracted){
+            console.log(`The link in list with length ${linksExtracted.length} and type ${typeof(linksExtracted)} is: `, link)
+            
+            const httpResponse = await fetch(link).then(
+                objectResponse => {
+                   if (objectResponse.ok){
+                    console.log(`The link ${link} is OK`)
+                   }else{
+                    listNotExists.push()
+                    return listNotExists
+                   }
+                }
+            )
+        }
+    }catch(errorObject){
+        console.log('Deu erro aqui', errorObject.stack)
+    }
+
 }
