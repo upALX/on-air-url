5284110a826d7e090c4c61d28fe5992b0b0a27c9
Alexandre <alexandreaddres@gmail.com>
2023-09-26 10:43:40 -0300

    adding links validation tested

--- src/validate-url.js
+++ src/validate-url.js
@@ -12,7 +12,7 @@ export default async function validateLinks(links){
         for (let link of linksExtracted){
             console.log(`The link in list with length ${linksExtracted.length} and type ${typeof(linksExtracted)} is: `, link)
             
-            const httpResponse = await fetch(link).then(
+            const httpResponse = await fetch(link, {method: 'HEAD'}).then(
                 objectResponse => {
                    if (objectResponse.ok){
                     console.log(`The link ${link} is OK`)
